37b1cfba1bb59304e0d38dfebb10feaa299ba2bb
Park-jaehyeon02 <1234qkr@gmail.com>
2022-11-06 13:46:35 +0900

    add entities

---
+++ entities/product.js
@@ -0,0 +1,21 @@
+class Product {
+  constructor({ id, name, category, price }) {
+    hit.id = id;
+    this.name = name;
+    this.category = category;
+    this.price = price;
+  }
+
+  //   constructor(value) {
+  //     this.name = value.name;
+  //     this.category = value.category;
+  //     this.price = value.price;
+  //   }
+}
+
+// const p1 = new Product({
+//   id: 'sch_test',
+//   name: 'test name',
+//   category: 'food',
+//   price: 2000,
+// });
